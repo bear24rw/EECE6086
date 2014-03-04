7dbb612726e5c243270f88723db27bc73aeed3be
Max Thrun <bear24rw@gmail.com>
2014-03-04 04:57:42 -0500

    reduce duplicate cell constructor code

--- cell.cpp
+++ cell.cpp
@@ -2,24 +2,16 @@
 #include "term.h"
 
 cell_t::cell_t() {
-    number = 0;
-    flip_x = false;
-    flip_y = false;
-    feed_through = false;
-    terms.resize(4);
-    for (int t=0; t<4; t++) {
-        terms[t].cell = this;
-        terms[t].number = t;
-    }
+    cell_t(false);
 }
 
 cell_t::cell_t(bool ft) {
     number = 0;
     flip_x = false;
     flip_y = false;
-    feed_through = true;
-    terms.resize(2);
-    for (int t=0; t<2; t++) {
+    feed_through = ft;
+    ft ? terms.resize(2) : terms.resize(4);
+    for (unsigned int t=0; t<terms.size(); t++) {
         terms[t].cell = this;
         terms[t].number = t;
     }
