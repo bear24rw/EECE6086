2d8a80b59909ef4677d88d3a3aaa0ed1be297283
Max Thrun <bear24rw@gmail.com>
2014-03-04 20:11:42 -0500

    svg working

--- mag_to_svg.py
+++ mag_to_svg.py
@@ -1,7 +1,7 @@
 import sys
 import svgwrite
 
-pixels_per_grid = 1
+pixels_per_grid = 10
 
 materials = {}
 materials['metal1'] = 'blue'
@@ -11,17 +11,23 @@ materials['default'] = 'black'
 infile = open(sys.argv[1])
 outfile = sys.argv[1].rsplit('.', 1)[0] + ".svg"
 
-dwg = svgwrite.Drawing(outfile, size = ("1000px", "1000px"), viewBox=('0 0 1000 1000'))
+texts = []
+
+dwg = svgwrite.Drawing(outfile, size = ("1000px", "1000px"), viewBox=('-50 -50 1000 1000'))
 
 current_material = 'black'
 
-def rect(x1, y1, x2, y2, stroke_width="0", fill_opacity=0.5, stroke="black"):
-    x1 *= pixels_per_grid
-    y1 *= pixels_per_grid
-    x2 *= pixels_per_grid
-    y2 *= pixels_per_grid
-    dwg.add(dwg.rect(insert = (x1, y1),
-                       size = (x2, y2),
+for i in range(100):
+    dwg.add(dwg.line(start=(i*pixels_per_grid,0), end=(i*pixels_per_grid,1000), stroke='gray', stroke_width="1px"))
+    dwg.add(dwg.line(start=(0,i*pixels_per_grid), end=(1000,i*pixels_per_grid), stroke='gray', stroke_width="1px"))
+
+def rect(x, y, w, h, stroke_width="0", fill_opacity=0.5, stroke="black"):
+    x *= pixels_per_grid
+    y *= pixels_per_grid
+    w *= pixels_per_grid
+    h *= pixels_per_grid
+    dwg.add(dwg.rect(insert = (x, y),
+                       size = (w, h),
                        stroke_width = stroke_width,
                        stroke = stroke,
                        fill_opacity = fill_opacity,
@@ -42,7 +48,7 @@ for line in infile:
         y1 = int(y1)
         y2 = int(y2)
         print "rect: %d %d %d %d" % (x1, y1, x2, y2)
-        rect(x1, y2, x2-x1, y2-y1)
+        rect(x1, y1, x2-x1, y2-y1)
         continue
     if line.startswith("transform"):
         _, t1, t2, cur_x, t3, t4, cur_y = line.strip().split()
@@ -51,18 +57,31 @@ for line in infile:
         continue
     if line.startswith("box"):
         _, x1, y1, x2, y2 = line.strip().split()
-        x1 = int(x1)
-        x2 = int(x2)
-        y1 = int(y1)
-        y2 = int(y2)
-        rect(cur_x+x1, cur_y+y2, x2-x1, y2-y1, stroke_width="1px", fill_opacity=0)
+        x1 = min(int(x1), int(x2))
+        y1 = min(int(y1), int(y2))
+        x2 = max(int(x1), int(x2))
+        y2 = max(int(y1), int(y2))
+        rect(cur_x+x1, cur_y+y1, x2-x1, y2-y1, stroke_width="5px", fill_opacity=0)
+        continue
+    if line.startswith("rlabel"):
+        _, mat, x1, y1, x2, y2, pos, text = line.strip().split()
+        x1 = min(int(x1), int(x2))
+        y1 = min(int(y1), int(y2))
+        x2 = max(int(x1), int(x2))
+        y2 = max(int(y1), int(y2))
+        x = (x2-x1)/2.0+x1
+        y = (y2-y1)/2.0+y1
+        x *= pixels_per_grid
+        y *= pixels_per_grid
+        print "text: %d %d" % (x,y)
+        t = dwg.text(text, insert=(x,y), fill="white")
+        t["font-size"] = 10
+        t["text-anchor"] = "middle"
+        t["dominant-baseline"] = "central"
+        texts.append(t)
         continue
 
-"""
-dwg.add(dwg.rect(insert = (-50, -50),
-                   size = (100, 100),
-                   stroke_width = "1",
-                   stroke = "black",
-                   fill = 'green'))
-"""
+for t in texts:
+    dwg.add(t)
+
 dwg.save()
