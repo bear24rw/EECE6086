f308036b6bbc709cc3b48b9db0ff5d2fb30563c2
Max Thrun <bear24rw@gmail.com>
2014-04-15 12:12:18 -0400

    works

--- hw_3/main.c
+++ hw_3/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/time.h>
 #include <getopt.h>
+#include <limits.h>
 //#include "main.h"
 
 int num_vars;
@@ -33,37 +34,62 @@ int find_most_binate(char **vector)
 {
     int comp_form = 0;
     int true_form = 0;
-    int max_binate = 0;
-    int binate_var = 0;
-    int temp_binate = 0;
+    int min_column = 0;
+    int min_difference = INT_MAX;
 
     for (int j=0; j<num_vars; j++) {
-        for (int i=0; i<num_cubes; i++) {
-            if (vector[i][j] == 1) true_form+=1;
-            if (vector[i][j] == 0) comp_form+=1;
-            printf("%c", vector[i][j]);
+
+        true_form = 0;
+        comp_form = 0;
+
+        // total up the number of 1s and 0s in this column
+        for (int i=0; i<num_cubes && vector[i] != NULL; i++) {
+            if (vector[i][j] == '1') true_form++;
+            if (vector[i][j] == '0') comp_form++;
         }
-        // check if polarity changes, if not then the variable is found to be
-        // unate, continue checking if other variables are binate.
-        if ((true_form >  0 && comp_form == 0) ||
-            (true_form == 0 && comp_form >  0))
+
+        if (true_form == 0 || comp_form == 0)
             continue;
-        temp_binate = abs(true_form - comp_form);
-        if (max_binate > temp_binate) {
-            max_binate = temp_binate;
-            binate_var = j;
+
+        int difference = abs(true_form - comp_form);
+        if (difference < min_difference) {
+            min_difference = difference;
+            min_column = j;
         }
-        printf("\n");
     }
-    printf("Most binate variable is %d with a value of: %d\n", binate_var, max_binate);
-    return binate_var;
+
+    return min_column;
+}
+
+char **co_factor(char **vector, int column, char pc)
+{
+    char **temp_vector = (char **)malloc(num_cubes*sizeof(char*));
+
+    for (int i=0; i<num_cubes; i++) {
+        temp_vector[i] = (char *)malloc(num_vars);
+    }
+
+    int row = 0;
+
+    for (int i=0; i<num_cubes && vector[i] != NULL; i++) {
+
+        if (vector[i][column] != pc && vector[i][column] != '-') continue;
+
+        memcpy(temp_vector[row], vector[i], num_vars*sizeof(char));
+
+        temp_vector[row][column] = '-';
+
+        row++;
+    }
+
+    //printf("co_factor rows: %d column: %d pc: %c\n", row, column, pc);
+    temp_vector[row] = NULL;
+
+    return temp_vector;
 }
 
 //http://cc.ee.ntu.edu.tw/~jhjiang/instruction/courses/fall10-lsv/lec03-2_2p.pdf
-//int check_tautology(char **vector, int binate_var, int p_flag)
-//int count = 0;
-//int binate_var = 0;
-int check_tautology(char **vector, char **temp_minterms, int pc)
+int check_tautology(char **vector)
 {
     /*
      positive cofactor (x = 1):
@@ -78,106 +104,34 @@ int check_tautology(char **vector, char **temp_minterms, int pc)
     */
 
     // check to see if we have run out of cubes
-    //if (count)
-    //if (temp_minterms == NULL) return 0;
-    if (vector == NULL) return 0;
+    if (vector[0] == NULL) return 0;
 
     // check to see if cube consists of all dashes, if so then we have a
     // tautology
-    for (int i=0; i<num_cubes; i++) {
-        //if (count) {
-            //if (all_dash(temp_minterms[i])) {
-            if (all_dash(vector[i])) {
-                printf("Function is a tautololgy\n");
-                return 1;
-            }
-        //}
+    for (int i=0; i<num_cubes && vector[i] != NULL; i++) {
+        if (all_dash(vector[i])) {
+            return 1;
+        }
     }
 
     // pick most binate variable to check for tautology
-    /*
-    if (count)
-        binate_var = find_most_binate(temp_minterms);
-    else {
-        count = 1;
-        binate_var = find_most_binate(vector);
-    }
-    */
     int binate_var = find_most_binate(vector);
 
-    /*
-    int positive_cofactor = cofactor(vector, 1);
-    if (check_tautology(positive_cofactor) == False)
-        return False
-
-    int negative_cofactor = cofactor(vector, 0);
-    if (check_tautology(negative_cofactor) == False)
-        return False
-    */
-
-    // check for tautology on the positive cofactor of most binate variable
-    if (pc) {
-        for (int i=0; i<num_cubes; i++) {
-            if (vector[i][binate_var] == '1') {
-                printf("%c\n", vector[i][binate_var]);
-
-                memcpy(temp_minterms[i], vector[i], num_vars*sizeof(char*));
-                temp_minterms[i][binate_var] = '-';
-
-                printf("%s\n", vector[i]);
-                printf("%s\n", temp_minterms[i]);
-                /*
-                if (count) {
-                    temp_minterms[i] = temp_minterms[i];
-                    temp_minterms[i][binate_var] = '-';
-                }else{
-                    temp_minterms[i] = vector[i];
-                    temp_minterms[i][binate_var] = '-';
-                }
-                */
-            }
-        }
-        // would be nice to remove all "counts" from above and just have
-        // temp_minterms overwrite the vector but i don't think i want to lose
-        // 'vector' so...
-        if (check_tautology(temp_minterms, temp_minterms, 1)) return 1;
-        //if (check_tautology(vector, temp_minterms, 1)) return 1;
+    char **C0 = co_factor(vector, binate_var, '0');
+    if (!check_tautology(C0)){
+        return 0;
     }
 
-    // check for tautology on the negative cofactor of most binate variable
-    if (!pc) {
-        for (int i=0; i<num_cubes; i++) {
-            if (vector[i][binate_var] == '0') {
-                printf("%c\n", vector[i][binate_var]);
-
-                memcpy(temp_minterms[i], vector[i], num_vars*sizeof(char*));
-                temp_minterms[i][binate_var] = '-';
-
-                printf("%s\n", vector[i]);
-                printf("%s\n", temp_minterms[i]);
-                /*
-                if (count) {
-                    temp_minterms[i] = temp_minterms[i];
-                    temp_minterms[i][binate_var] = '-';
-                }else{
-                    temp_minterms[i] = vector[i];
-                    temp_minterms[i][binate_var] = '-';
-                }
-                */
-            }
-        }
-        if (check_tautology(temp_minterms, temp_minterms, 0)) return 1;
+    char **C1 = co_factor(vector, binate_var, '1');
+    if (!check_tautology(C1)){
+        return 0;
     }
-    /*
-    */
 
-    return 0;
+    return 1;
 }
 
 int main(int argc, char *argv[])
 {
-    //start_time = std::clock();
-
     //
     // Get number of variables and cubes
     //
@@ -198,48 +152,26 @@ int main(int argc, char *argv[])
 
     printf("Found %d variables and %d cubes\n", num_vars, num_cubes);
 
-    char **minterms      = (char **)malloc(num_cubes*sizeof(char*));
-    char **temp_minterms = (char **)malloc(num_cubes*sizeof(char*));
-    char **temp_vector   = (char **)malloc(num_cubes*sizeof(char*));
+    char **vector = (char **)malloc(num_cubes*sizeof(char*));
 
     for (int i=0; i<num_cubes; i++) {
-        minterms[i]      = (char *)malloc(num_vars);
-        temp_minterms[i] = (char *)malloc(num_vars);
-        temp_vector[i]   = (char *)malloc(num_vars);
-        //minterms[i] = (char *)malloc((num_vars+1)*sizeof(char));
+        vector[i] = (char *)malloc(num_vars);
     }
 
     for (int i=0; i<num_cubes; i++) {
-        fscanf(fp, "%s", minterms[i]);
-        replace_under_with_dash(minterms[i]);
-        // TODO: might remove this since i'm doing it in the 'check tautology
-        // func'
-        if (all_dash(minterms[i])) {
+        fscanf(fp, "%s", vector[i]);
+        replace_under_with_dash(vector[i]);
+        if (all_dash(vector[i])) {
             printf("Function is a tautololgy\n");
             return 0;
         }
     }
 
-    for (int i=0; i<num_cubes; i++) {
-        memcpy(temp_vector[i], minterms[i], num_vars*sizeof(char *));
+    if (check_tautology(vector)) {
+        printf("Function is a tautololgy\n");
+    } else {
+        printf("Function is NOT a tautololgy\n");
     }
 
-    //int positive_cofactor = check_tautology(minterms, temp_minterms, 1);
-    int positive_cofactor = check_tautology(temp_vector, temp_minterms, 1);
-
-    if (!positive_cofactor) {
-        printf("Function is not a tautololgy\n");
-        return 0;
-    }
-
-    // pretty sure by the time i pass in temp_minterms, it has already been modified....
-    // i really don't want to make 100000000000 copies of the matrix, so yeah...
-    int negative_cofactor = check_tautology(minterms, temp_minterms, 0);
-    if (!negative_cofactor) {
-        printf("Function is not a tautololgy\n");
-        return 0;
-    }
-
-    printf("Function is a tautololgy\n");
     return 0;
 }
